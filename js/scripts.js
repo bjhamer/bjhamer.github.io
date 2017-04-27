4da2aa5e1d590edb0d29c5b481dbcb18ce0f695a
Brad Hamer <BHamer477@gmail.com>
2017-04-27 15:28:37 -0500

    Smooth Scrolling and Form Styling
    added smooth scrolling for links and added spacing and width for contact
    form

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,17 @@ window.addEventListener('scroll', function() {
     $('#top').css("display", "none");
   }
 });
+
+$(document).ready(function() {
+  $("a").on('click', function(event) {
+    if(this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function() {
+        window.location.hash = hash;
+      });
+    }
+  });
+});
